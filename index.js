f1a308b5b86c382c57ca4672eea116e7779050b4
nykanenj <juusonykanen@hotmail.com>
2020-04-15 05:22:02 +0300

    added routes and middleware and postman export

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use(cors());
 
 let notes = [
   {
@@ -20,17 +26,63 @@ let notes = [
     date: "2020-01-10T19:20:14.298Z",
     important: true
   }
-]
+];
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0;
+  return maxId + 1;
+};
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
+  res.send('<h1>Hello World!</h1>');
+});
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes)
-})
+  res.json(notes);
+});
 
-const PORT = 3001
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find(note => note.id === id);
+  
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+});
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    });
+  };
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter(note => note.id !== id);
+
+  response.status(204).end();
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`);
 })
